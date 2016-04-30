0392c25d88d532defdf082ab407e9d4d099a1d68
Melinda N. Carlson <mcarlson@bitmosphere.com>
2016-04-30 11:43:18 -0400

    Dictionary with key-value pairs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,22 +10,47 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            List<int> myList = new List<int>();
-            myList.Add(3);
-            myList.Add(14);
-            myList.Add(71);
-            Console.Write ("List: ");
+            // First, try with key => value pairs.  
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("tiger", 32);
+            myDictionary.Add("grass", 1);
+            myDictionary.Add("lamp", 20);
+            Console.Write("Dictionary: ");
 
-            for (int i = 1; i < myList.Count; i++)
+            foreach(var key in myDictionary)
             {
-                Console.Write( "{0}, ", myList[i]);
+                Console.Write(key + " ");
+            }
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine("For key = \"tiger\", value = {0}.", myDictionary["tiger"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key not found.");
             }
 
-            Console.WriteLine("{0}.", myList.Last());
             Console.ReadKey();
            
         }
 
-       
+        public void ListTest()
+        {
+            List<int> myList = new List<int>();
+            myList.Add(3);
+            myList.Add(14);
+            myList.Add(71);
+            Console.Write("List: ");
+
+            for (int i = 1; i < myList.Count; i++)
+            {
+                Console.Write("{0}, ", myList[i]);
+            }
+
+            Console.WriteLine("{0}.", myList.Last());
+            Console.ReadKey();
+        }
     }
 }
