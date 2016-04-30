5dc7ab2ecefb49dc95d03b434a6de3e073e3b9e4
Melinda N. Carlson <mcarlson@bitmosphere.com>
2016-04-30 11:09:02 -0400

    generate a list

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            myList.Add(3);
+            myList.Add(14);
+            myList.Add(71);
+            Console.Write ("List: ");
+
+            for (int i = 1; i < myList.Count; i++)
+            {
+                Console.Write( "{0}, ", myList[i]);
+            }
+
+            Console.WriteLine("{0}.", myList.Last());
+            Console.ReadKey();
+           
+        }
+
+       
+    }
+}
